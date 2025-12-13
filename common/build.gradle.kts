9034d76b8942b47a85da1f4ccdd664c5a409ba8b
Junhyung Im <entrypointkr@gmail.com>
2025-12-13 15:03:29 +0900

    chore: remove signing plugin declare common/build.gradle

--- common/build.gradle.kts
+++ common/build.gradle.kts
@@ -1,8 +1,6 @@
 plugins {
     `kotlin-dsl`
     `version-catalog`
-    `maven-publish`
-    signing
     id("spigradle-versioning")
     id("spigradle-java")
     id("io.typst.spigradle.central.publish") // sourced in build-logic/central-publish
