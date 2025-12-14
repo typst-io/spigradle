adfcc400b109e6f2609567b9771f70bc63bb57d8
Junhyung Im <entrypointkr@gmail.com>
2025-12-14 15:47:02 +0900

    chore: fix compile error

--- build-logic/build.gradle.kts
+++ build-logic/build.gradle.kts
@@ -1,3 +1,17 @@
+import java.util.*
+
+// NOTE: https://github.com/gradle/gradle/issues/21144
+val rootProperties: Properties = Properties().apply {
+    load(file("../gradle.properties").bufferedReader())
+}
+
 allprojects {
     group = "io.typst"
+
+    pluginManager.withPlugin("java") {
+        val java = extensions["java"] as JavaPluginExtension
+        java.toolchain {
+            languageVersion.set(JavaLanguageVersion.of(rootProperties.getProperty("java.version").toInt()))
+        }
+    }
 }
